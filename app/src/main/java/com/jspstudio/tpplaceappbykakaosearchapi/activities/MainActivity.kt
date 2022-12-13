95dc75b655b9fe3f4494ab252f3ab373d8bf9ba4
jjeansp9 <jjeansp9@gmail.com>
2022-12-13 17:18:05 +0900

    commit

--- app/src/main/java/com/jspstudio/tpplaceappbykakaosearchapi/activities/MainActivity.kt
+++ app/src/main/java/com/jspstudio/tpplaceappbykakaosearchapi/activities/MainActivity.kt
@@ -2,12 +2,63 @@ package com.jspstudio.tpplaceappbykakaosearchapi.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import com.google.android.material.tabs.TabLayout
+import com.jspstudio.tpplaceappbykakaosearchapi.R
+import com.jspstudio.tpplaceappbykakaosearchapi.databinding.ActivityMainBinding
+import com.jspstudio.tpplaceappbykakaosearchapi.fragments.SearchListFragment
+import com.jspstudio.tpplaceappbykakaosearchapi.fragments.SearchMapFragment
+
 //import com.jspstudio.tpplaceappbykakaosearchapi.R
 
 class MainActivity : AppCompatActivity() {
 
+    val binding:ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        // 툴바를 제목줄로 대체하여 설정
+        setSupportActionBar(binding.toolbar)
+
+        // 첫 실행될 프래그먼트를 동적 추가
+        supportFragmentManager.beginTransaction().add(R.id.container_fragment, SearchListFragment()).commit()
+
+        // 탭레이아웃의 탭버튼 클릭시 보여줄 프래그먼트를 변경
+        binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                if (tab?.text == "LIST"){
+                    supportFragmentManager.beginTransaction().replace(R.id.container_fragment, SearchListFragment()).commit()
+                }else if(tab?.text == "MAP"){
+                    supportFragmentManager.beginTransaction().replace(R.id.container_fragment, SearchMapFragment()).commit()
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+
+        })
+
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.option_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.menu_help-> Toast.makeText(this, "도움말", Toast.LENGTH_SHORT).show()
+            R.id.menu_logout -> Toast.makeText(this, "로그아웃", Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
---
+++ app/src/main/java/com/jspstudio/tpplaceappbykakaosearchapi/fragments/SearchListFragment.kt
@@ -0,0 +1,21 @@
+package com.jspstudio.tpplaceappbykakaosearchapi.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.jspstudio.tpplaceappbykakaosearchapi.databinding.FragmentSearchListBinding
+
+class SearchListFragment : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return binding.root
+    }
+
+    val binding:FragmentSearchListBinding by lazy { FragmentSearchListBinding.inflate(layoutInflater) }
+
+}
